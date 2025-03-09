7f41a20d397aea4ec84050badf508438bc7a1024
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-03-09 12:08:18 +0100

    Adding comments to the bullet script

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    //If the bullet hits something, it will be destroyed. Other things can happen depending on what you hit
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Enemy"))

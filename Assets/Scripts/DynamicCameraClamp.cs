c9e8f27456c049530c08e9f9721284f5b871d444
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-03-09 12:50:22 +0100

    Adding comments to the dynamic clamp

--- Assets/Scripts/DynamicCameraClamp.cs
+++ Assets/Scripts/DynamicCameraClamp.cs
@@ -2,29 +2,36 @@ using UnityEngine;
 
 public class DynamicCameraClamp : MonoBehaviour
 {
+    //The room's maximum and minimum camera clamps
     public Vector3 cameraClampMin;
     public Vector3 cameraClampMax;
 
-    private GameObject playerCamera;
+    //A refrence to the camera script
+    private CameraScript playerCamera;
 
+    //A bool for if the camera should change its height
     public bool changesHeight;
 
+    //The new height at which the camera should be in
     public float yOffsetChange;
 
     private void Start()
     {
-        playerCamera = GameObject.Find("Main Camera");
+        //Assigning the camera script
+        playerCamera = GameObject.Find("Main Camera").GetComponent<CameraScript>();
     }
 
     private void OnTriggerStay(Collider other)
     {
+        //If the player enters the trigger, the clamped position of the camera will change
         if(other.CompareTag("Player"))
         {
-            playerCamera.GetComponent<CameraScript>().minCamPosition = cameraClampMin;
-            playerCamera.GetComponent<CameraScript>().maxCamPosition = cameraClampMax;
+            playerCamera.minCamPosition = cameraClampMin;
+            playerCamera.maxCamPosition = cameraClampMax;
+            //If the camera is supposed to change its height, it will do so when the player enters the trigger
             if(changesHeight)
             {
-                playerCamera.GetComponent<CameraScript>().offset.y = yOffsetChange;
+                playerCamera.offset.y = yOffsetChange;
             }
         }
     }

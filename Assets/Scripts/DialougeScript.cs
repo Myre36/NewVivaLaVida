f6873c838eb9f7405ad6d3fbe69245fe9c6a5ee7
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-03-09 12:30:04 +0100

    Added comments to the dialouge script

--- Assets/Scripts/DialougeScript.cs
+++ Assets/Scripts/DialougeScript.cs
@@ -4,29 +4,37 @@ using UnityEngine.UI;
 
 public class DialougeScript : MonoBehaviour
 {
-    //
-    public GameObject dialougeBox;
+    //A refrence to the dialouge box
+    public RawImage dialougeBox;
 
+    //A refrence to the dialouge text
     public TMP_Text dialougeText;
 
+    //An array of all the dialouge that will be displayed
     public string[] dialouge;
 
+    //A bool to check if the player is in range of the object
     public bool playerInRange = false;
+    //A bool for if the object should have an outline or not
     public bool outlineEneabled = false;
+    //A bool for if the object is a book or not
     public bool isBook;
 
+    //The current line in the dialouge
     public int lineNumber;
 
     private void Start()
     {
+        //If the object is a book, the book sprite will be assigned as the dialouge box
         if(isBook)
         {
-            dialougeBox = GameObject.Find("UIBook");
+            dialougeBox = GameObject.Find("UIBook").GetComponent<RawImage>();
             dialougeText = GameObject.Find("UIBookText").GetComponent<TMP_Text>();
         }
+        //If not, then the normal dialouge box is used
         else
         {
-            dialougeBox = GameObject.Find("DialougeBox");
+            dialougeBox = GameObject.Find("DialougeBox").GetComponent<RawImage>();
             dialougeText = GameObject.Find("DialougeText").GetComponent<TMP_Text>();
         }
     }
@@ -34,17 +42,20 @@ public class DialougeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //If the player is in range of the object and presses the interact key
         if(playerInRange)
         {
             if((Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.JoystickButton1)))
             {
+                //If the player has not reached the end of the dialouge, the next line will be displayed
                 if(lineNumber < dialouge.Length)
                 {
                     NextLine();
                 }
+                //If the player has reached the end of the dialouge, the dialouge will close
                 else
                 {
-                    dialougeBox.GetComponent<RawImage>().enabled = false;
+                    dialougeBox.enabled = false;
                     dialougeText.enabled = false;
                     lineNumber = 0;
                 }
@@ -52,6 +63,7 @@ public class DialougeScript : MonoBehaviour
         }
     }
 
+    //If the player enters the object's trigger, it will register that the player is in there and the outline will turn on if allowed
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -64,12 +76,13 @@ public class DialougeScript : MonoBehaviour
         }
     }
 
+    //If the player exits the object's trigger, it will register that the player is not there and the dialouge will turn off
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerInRange = false;
-            dialougeBox.GetComponent<RawImage>().enabled = false;
+            dialougeBox.enabled = false;
             dialougeText.enabled = false;
             if (outlineEneabled)
             {
@@ -78,9 +91,10 @@ public class DialougeScript : MonoBehaviour
         }
     }
 
+    //A function for displaying the next dialouge line
     void NextLine()
     {
-        dialougeBox.GetComponent<RawImage>().enabled = true;
+        dialougeBox.enabled = true;
         dialougeText.enabled = true;
         dialougeText.text = dialouge[lineNumber];
         lineNumber++;

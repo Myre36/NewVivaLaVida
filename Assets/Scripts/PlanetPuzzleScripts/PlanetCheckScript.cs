83d6371e1a09d12c4a807b5f6dfabe19fce8d577
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-03-09 11:36:16 +0100

    Added comments to the planet checker

--- Assets/Scripts/PlanetPuzzleScripts/PlanetCheckScript.cs
+++ Assets/Scripts/PlanetPuzzleScripts/PlanetCheckScript.cs
@@ -2,25 +2,32 @@ using UnityEngine;
 
 public class PlanetCheckScript : MonoBehaviour
 {
+    //A bool for when all planets are in order
     public bool planetsInOrder;
 
+    //An array of all the stands
     public GameObject[] stands;
-
+    //A refrence to the key that will appear when the puzzle is solved
     public GameObject keyItem;
 
+    //A refrence to the game manager
     private GameManager gameManager;
 
     private void Start()
     {
+        //Assigning the game manager
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Checks if all stands have the correct planets placed on them
         if (stands[0].GetComponent<PanetPuzzleScript>().hasCorrectPlanet && stands[1].GetComponent<PanetPuzzleScript>().hasCorrectPlanet && stands[2].GetComponent<PanetPuzzleScript>().hasCorrectPlanet && stands[3].GetComponent<PanetPuzzleScript>().hasCorrectPlanet && stands[4].GetComponent<PanetPuzzleScript>().hasCorrectPlanet && stands[5].GetComponent<PanetPuzzleScript>().hasCorrectPlanet && stands[6].GetComponent<PanetPuzzleScript>().hasCorrectPlanet && stands[7].GetComponent<PanetPuzzleScript>().hasCorrectPlanet)
         {
+            //Sets the bool to true
             planetsInOrder = true;
+            //Goes through all stands and tells all of them that the puzzle has been solved
             for(int i = 0; i < stands.Length; i++)
             {
                 var currentStand = stands[i].GetComponent<PanetPuzzleScript>();
@@ -28,6 +35,7 @@ public class PlanetCheckScript : MonoBehaviour
                 currentStand.playerInRange = false;
             }
         }
+        //When the planets are in the correct order, the key is turned on and this script is deactivated
         if(planetsInOrder)
         {
             keyItem.SetActive(true);

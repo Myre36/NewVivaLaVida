30da44d006c12190e27a1cca619040fa419a7a29
Maya <mayavenas@gmail.com>
2025-03-13 10:13:30 +0100

    Added a temorary map open and close sound, will be updated towards the end

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MapManager : MonoBehaviour
 {
     [SerializeField] private GameObject firstFloorMap;
     [SerializeField] private GameObject secondFloorMap;
+    [SerializeField] private AudioClip openMapSound;
+    [SerializeField] private AudioClip closeMapSound;
 
     private GameObject currentMap;
 
@@ -14,7 +17,12 @@ public class MapManager : MonoBehaviour
     public bool firstFloor;
 
     public Animator mapAnimator;
+    private AudioSource audioSource;
 
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void Update()
     {
@@ -44,18 +52,31 @@ public class MapManager : MonoBehaviour
 
     IEnumerator OpenMap()
     {
+         if (openMapSound != null)
+        {
+            audioSource.PlayOneShot(openMapSound);
+        }
         mapIsOpen = true;
         mapAnimator.SetBool("IsOpen", true);
         yield return new WaitForSeconds(1f);
         currentMap.SetActive(true);
+
+       
     }
 
 
     IEnumerator CloseMap()
     {
+        if (closeMapSound != null)
+        {
+            audioSource.PlayOneShot(closeMapSound);
+        }
+        
         mapIsOpen = false;
         mapAnimator.SetBool("IsOpen", false);
         yield return new WaitForSeconds(0.1f);
         currentMap.SetActive(false);
+
+        
     }
 }

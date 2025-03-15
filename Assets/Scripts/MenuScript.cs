89076f2190f302eb0044e83127e48d2b9ac56eb7
Myre36 <67600948+Myre36@users.noreply.github.com>
2025-03-15 20:07:23 +0100

    Fixing some minor issues with menus

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -22,7 +22,7 @@ public class MenuScript : MonoBehaviour
 
     public void LoadMenu()
     {
-        SceneManager.LoadScene("TutorialScene");
+        SceneManager.LoadScene("Main Menu");
     }
 
     public void LoadIntro()
@@ -33,5 +33,15 @@ public class MenuScript : MonoBehaviour
     public void LoadCreditsScene()
     {
         SceneManager.LoadScene("CreditsScene");
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }

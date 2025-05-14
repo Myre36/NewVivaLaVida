850c1eced3896f5ef0ab56dba11e482125417d67
Maya <mayavenas@gmail.com>
2025-05-14 11:06:32 +0200

    chest script changes

--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -215,6 +215,7 @@ public class ChestScript : MonoBehaviour
                         {
                             gameManager.chest2 = true;
                             UnlockChest();
+                            Destroy(CrownChest);
                         }
                         else
                         {
